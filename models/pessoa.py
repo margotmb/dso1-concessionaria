c99e56c7ff39a86c05601e287b693359210bd265
Margot <margot.mb@outlook.com>
2021-03-07 21:17:21 -0300

    Atributos: Abstract Pessoa -> Cliente/Vendedor

--- models/pessoa.py
+++ models/pessoa.py
@@ -4,5 +4,7 @@ from abc import ABC, abstractmethod
 class Pessoa(ABC):
 
     @abstractmethod
-    def __init__(self):
-        pass
+    def __init__(self, nome: str, cpf: str, telefone: str):
+        self.__nome = nome
+        self.__cpf = cpf
+        self.__telefone = telefone
--- models/vendedor.py
+++ models/vendedor.py
@@ -3,5 +3,5 @@ from pessoa import Pessoa
 
 class Vendedor(Pessoa):
 
-    def __init__(self):
-        pass
+    def __init__(self, nome: str, cpf: str, telefone: str):
+        super().__init__(nome, cpf, telefone)
