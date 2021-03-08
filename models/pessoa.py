62d25c96c6cff32b91e5509a558c97ff269c6ab0
Margot <margot.mb@outlook.com>
2021-03-07 21:49:25 -0300

    Abstract: Pessoa -> Gets/Sets

--- models/pessoa.py
+++ models/pessoa.py
@@ -8,3 +8,28 @@ class Pessoa(ABC):
         self.__nome = nome
         self.__cpf = cpf
         self.__telefone = telefone
+
+    @property
+    def nome(self):
+        return self.__nome
+  
+    @nome.setter
+    def nome(self, nome: str):
+        self.__nome = nome
+
+    @property
+    def cpf(self):
+        return self.__cpf
+
+    @cpf.setter
+    def cpf(self, cpf: str):
+        self.__cpf = cpf
+
+    @property
+    def telefone(self):
+        return self.__telefone
+  
+    @telefone.setter
+    def telefone(self, telefone: str):
+        self.__telefone = telefone
+
