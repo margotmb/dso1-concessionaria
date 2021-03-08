3c86a491de3a4da67e4bd493830ad3c264ab0aa2
Margot <margot.mb@outlook.com>
2021-03-07 21:03:06 -0300

    Carro: Gets/Sets

--- models/carro.py
+++ models/carro.py
@@ -1,4 +1,30 @@
 class Carro:
 
-    def __init__(self):
-        pass
+    def __init__(self, modelo: str, ano: int, valor: float):
+        self.__modelo = modelo
+        self.__ano = ano
+        self.__valor = valor
+
+    @property
+    def modelo(self):
+        return self.__modelo
+    
+    @modelo.setter
+    def modelo(self, modelo: str):
+        self.__modelo = modelo
+
+    @property
+    def ano(self):
+        return self.__ano
+    
+    @ano.setter
+    def ano(self, ano: int):
+        self.__ano = ano
+
+    @property
+    def valor(self):
+        return self.__valor
+    
+    @valor.setter
+    def valor(self, valor: int):
+        self.__valor = valor
