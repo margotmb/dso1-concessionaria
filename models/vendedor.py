77f049af4ffab607bf0339e38a14ac09207453d9
Margot <margot.mb@outlook.com>
2021-03-19 17:51:58 -0300

    Atualizando Atributos

--- models/vendedor.py
+++ models/vendedor.py
@@ -5,3 +5,5 @@ class Vendedor(Pessoa):
 
     def __init__(self, nome: str, telefone: str):
         super().__init__(nome, telefone)
+        self.__carros_vendidos = 0
+        self.__receita_gerada = 0.0
