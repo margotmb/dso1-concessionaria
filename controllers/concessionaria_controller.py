01bbb4ed0bdfd5f0ac236448e53a734f4834c9f9
Margot <margot.mb@outlook.com>
2021-03-21 17:11:22 -0300

    Imports

--- controllers/concessionaria_controller.py
+++ controllers/concessionaria_controller.py
@@ -1,12 +1,14 @@
 from models.concessionaria import Concessionaria
 from views.concessionaria_view import ConcessionariaView
+from controllers.cliente_controller import ClienteController
+from controllers.vendedor_controller import VendedorController
 
 
 class ConcessionariaController:
 
     def __init__(self):
-        self.__model = Concessionaria()
-        self.__view =  ConcessionariaView()
+        self.__concessionaria_model = Concessionaria()
+        self.__concessionaria_view =  ConcessionariaView()
 
     def run(self):
         pass
