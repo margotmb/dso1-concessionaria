6287defa4cf0c9ddbe0be2827cee3aa26024d1e7
Margot <margot.mb@outlook.com>
2021-03-30 15:23:27 -0300

    Tela de Vendas

--- views/venda_view.py
+++ views/venda_view.py
@@ -1,3 +1,13 @@
+from datetime import date as Date
+
 class VendaView():
     def __init__(self):
         pass
+
+    def tela_de_vendas(self):
+        vendedor = int(input("ID do Vendedor: "))
+        cliente = int(input("ID do Cliente: "))
+        carro = int(input("ID do Carro: "))
+        tempo_garantia = int(input("Tempo de Garantia: "))
+        data = Date.today()
+        return [vendedor, cliente, carro, tempo_garantia, data]
