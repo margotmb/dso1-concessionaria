3b60d3359e3ddfc4defba07fc21721622dd4e57a
Margot <margot.mb@outlook.com>
2021-04-05 02:57:11 -0300

    CRUD Cliente

--- controllers/cliente_controller.py
+++ controllers/cliente_controller.py
@@ -3,21 +3,59 @@ from models.cliente import Cliente
 from views.cliente_view import ClienteView
 
 class ClienteController():
-    def __init__(self, model: Concessionaria):
-        self.__concessionaria_model = model
+    def __init__(self, concessionaria: Concessionaria):
+        self.__concessionaria = concessionaria
         self.__cliente_view = ClienteView()
 
+    #Tela Principal de Vendedor
     def run(self):
+        op_dict = {
+                "1" : self.cadastra,
+                "2" : self.lista,
+                "3" : self.atualiza,
+                "4" : self.remove
+        }
         opcao = self.__cliente_view.tela_principal()
+        while opcao != "0":
+            func = op_dict[opcao]
+            func()
+            opcao = self.__cliente_view.tela_principal()
 
-    def adiciona(self):
-        pass
+    def cadastra(self):
+        info = self.__cliente_view.cadastra()
+        if info is not None:
+            for cliente in self.__concessionaria.clientes:
+                if cliente.num_id == info[2]:
+                    self.__cliente_view.erro("Cliente já existe")
+                    return
+                if cliente.telefone == info[1]:
+                    self.__cliente_view.erro("Telefone já existe no sistema")
+                    return
+            cliente = Cliente(info[0], info[1], info[2])
+            self.__concessionaria.cadastra_objeto(cliente)
+            self.__cliente_view.sucesso()
 
     def lista(self):
-        pass
+        self.__cliente_view.lista(self.__concessionaria.clientes)
 
     def atualiza(self):
-        pass
+        self.lista()
+        identificacao = self.__cliente_view.cliente_id()
+        for cliente in self.__concessionaria.clientes:
+            if cliente.num_id == identificacao:
+                info = self.__cliente_view.atualiza()
+                if info is not None:
+                    cliente.nome = info[0]
+                    cliente.telefone = info[1]
+                    return
+        self.__cliente_view.erro("Cliente não encontrado")
 
     def remove(self):
-        pass
+        self.lista()
+        num_id = self.__cliente_view.remove()
+        for cliente in self.__concessionaria.clientes:
+            if cliente.num_id == num_id:
+                self.__concessionaria.remove_objeto(cliente)
+                self.__cliente_view.sucesso()
+                return
+        self.__cliente_view.erro("Cliente não encontrado")
--- views/cliente_view.py
+++ views/cliente_view.py
@@ -1,13 +1,78 @@
+import os
+
+
 class ClienteView():
     def __init__(self):
         pass
 
     def tela_principal(self):
-        print("Gerenciamento de clientes")
+        print("\n---- Gerenciamento de clientes ----")
         print("1 - Cadastrar")
         print("2 - Listar")
         print("3 - Atualizar")
         print("4 - Remover")
         print("0 - Sair")
-        opcao = input()
+        print("-----------------------------------")
+        opcao = input("Opção: ")
+        os.system('cls' if os.name == 'nt' else 'clear')
         return opcao
+
+    def cadastra(self):
+        print("-----Cadastramento de Cliente-----")
+        nome = input("Nome do Cliente:")
+        telefone = input("Telefone do Cliente:")
+        try:
+            num_id = int(input("Numero de Identificação:"))
+        except ValueError as e:
+            print('\nERRO: Caracter inválido: {}'.format(e))
+            return None
+        else:
+            if telefone.isdecimal():
+                os.system('cls' if os.name == 'nt' else 'clear')
+                return [nome, telefone, num_id]
+            else:
+                print("Telefone Inválido")
+
+    def lista(self, lista):
+        os.system('cls' if os.name == 'nt' else 'clear')
+        i = 0
+        print("\nLISTA DE CLIENTES:")
+        for cliente in lista:
+            print("#" + str(i))
+            print("Nome: " + cliente.nome)
+            print("Telefone: "+ cliente.telefone)
+            print("ID: "+ str(cliente.num_id))
+            print("-----------------------------------")
+            i += 1
+
+    def cliente_id(self):
+        try:
+            num_id = int(input("Digite o ID do cliente a ser atualizado:"))
+        except ValueError as e:
+            print('\nERRO: Caracter inválido: {}'.format(e))
+            return 0
+        else:
+            return num_id
+
+    def atualiza(self):
+        print("\n-------Atualização de Cliente--------")
+        nome = input("Nome do Cliente:")
+        telefone = input("Telefone do Cliente:")
+        os.system('cls' if os.name == 'nt' else 'clear')
+        return [nome, telefone]
+
+    def remove(self):
+        print("-----Remoção de Cliente-----")
+        try:
+            num_id = int(input("Digite o ID do cliente:"))
+        except ValueError as e:
+            print('\nERRO: Caracter inválido: {}'.format(e))
+            return 0
+        else:
+            return num_id
+
+    def sucesso(self):
+        print("Operação realizada com sucesso")
+    
+    def erro(self, mensagem_erro: str):
+        print("\n" + mensagem_erro)
