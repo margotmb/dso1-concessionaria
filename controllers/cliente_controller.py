be60684172e9a720deb36dac1d2563bca1799170
lorran-souza <lorransouzamch@gmail.com>
2021-04-05 15:12:13 -0300

    Atualização de controlador de cliente

--- controllers/cliente_controller.py
+++ controllers/cliente_controller.py
@@ -7,17 +7,56 @@ class ClienteController():
         self.__concessionaria_model = model
         self.__cliente_view = ClienteView()
 
+    #Tela Principal de Cliente
     def run(self):
+        op_dict = {
+                "1" : self.cadastra,
+                "2" : self.lista,
+                "3" : self.atualiza,
+                "4" : self.remove
+        }
         opcao = self.__cliente_view.tela_principal()
+        while opcao != "0":
+            func = op_dict[opcao]
+            func()
+            opcao = self.__cliente_view.tela_principal()
 
-    def adiciona(self):
-        pass
+    def cadastra(self):
+        info = self.__cliente_view.cadastra()
+        if info is not None:
+            for cliente in self.__concessionaria.clientes:
+                if cliente.num_id == info[2]:
+                    self.__cliente_view.erro("Cliente já existe")
+                    return
+                if cliente.telefone == info[1]:
+                    self.__cliente_view.erro("Telefone já existe no sistema")
+                    return
+            cliente = Cliente(info[0], info[1], info[2])
+            self.__concessionaria.cadastra_objeto(cliente)
+            self.__cliente_view.sucesso()
 
     def lista(self):
-        pass
+        self.__cliente_view.lista(self.__concessionaria.clientes)
 
     def atualiza(self):
-        pass
+        self.lista()
+        identificacao = self.__cliente_view.cliente_id()
+        for cliente in self.__concessionaria.clientes:
+            if cliente.num_id == identificacao:
+                info = self.__cliente_view.atualiza()
+                if info is not None:
+                    cliente.nome = info[0]
+                    cliente.telefone = info[1]
+                    self.__cliente_view.sucesso()
+                    return
+        self.__cliente_view.erro("Cliente não encontrado")
 
     def remove(self):
-        pass
+        self.lista()
+        num_id = self.__cliente_view.remove()
+        for cliente in self.__concessionaria.clientes:
+            if cliente.num_id == num_id:
+                self.__concessionaria.remove_objeto(cliente)
+                self.__cliente_view.sucesso()
+                return
+        self.__cliente_view.erro("Cliente não encontrado")
