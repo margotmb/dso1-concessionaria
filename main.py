0b597404ed47306330bcb9b07cd8a1912658a1cc
Margot <margot.mb@outlook.com>
2021-05-09 22:13:50 -0300

    Fix main

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from controllers.concessionaria_controller import ConcessionariaController
-import os
+from controllers.main_controller import MainController
 
 
-controlador = ConcessionariaController()
+controlador = MainController()
 controlador.run()
